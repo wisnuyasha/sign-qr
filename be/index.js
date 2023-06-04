452b98d2246ee40624f63158f472b91b9f975c77
RenDaemon <rendieyudh@gmail.com>
2023-06-04 11:25:12 +0700

    fix /sign bug

--- be/index.js
+++ be/index.js
@@ -10,6 +10,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
+app.use(express.json()); 
  // Add this line to parse JSON data
 
 app.get('/KeyGen', (req, res) => {
