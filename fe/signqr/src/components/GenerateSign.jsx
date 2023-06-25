0e8a86ce992b6f6ebc39cc5859171a884c88af65
treeoon <wisnuyeahfaizal@gmail.com>
2023-06-25 15:29:32 +0700

    ref: state on gensign

--- fe/signqr/src/components/GenerateSign.jsx
+++ fe/signqr/src/components/GenerateSign.jsx
@@ -1,37 +1,31 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-// import QRCode from "react-qr-code";
 import QRCode from "qrcode";
 
 export default function GenerateSignature() {
   const [inputFile, setInputFile] = useState(null);
-  const [isUpload, setIsUpload] = useState(false);
-  const [isUploadErr, setIsUploadErr] = useState(false);
-  const [inputPrivateKey, setInputPrivateKey] = useState("");
+  const [formData, setFormData] = useState({ inputPrivateKey: "", isUpload: false, isUploadErr: false });
   const [signature, setSignature] = useState("");
   const [signatureId, setSignatureId] = useState("");
   const [imageUrl, setImageUrl] = useState("");
   const [isGeneratedSign, setIsGeneratedSign] = useState(false);
-
+  
   async function genSign() {
-    if (isUpload) {
-      await axios
-        .post("http://localhost:5000/sign", {
-          privateKey: inputPrivateKey,
-        })
-        .then((res) => {
-          const getSignature = res.data.signature;
-          const getSignatureId = res.data.id;
-          setSignature(getSignature);
-          setSignatureId(getSignatureId);
-          setIsGeneratedSign(true);
-        })
-        .catch((err) => {
-          console.log(err);
+    if (formData.isUpload) {
+      try {
+        const res = await axios.post("http://localhost:5000/sign", {
+          privateKey: formData.inputPrivateKey,
         });
+        const { signature, id: signatureId } = res.data;
+        setSignature(signature);
+        setSignatureId(signatureId);
+        setIsGeneratedSign(true);
+      } catch (err) {
+        console.log(err);
+      }
     } else {
       console.log("please upload your file");
-      setIsUploadErr(true);
+      setFormData({...formData, isUploadErr: true})
     }
   }
 
@@ -45,6 +39,7 @@ export default function GenerateSignature() {
         return null;
       }
     };
+
     if (signatureId) {
       const qrUrl = `http://localhost:3000/verify/${signatureId}`;
       console.log(qrUrl);
@@ -59,14 +54,15 @@ export default function GenerateSignature() {
   async function handleFileUpload() {
     const formData = new FormData();
     formData.append("file", inputFile);
-    await axios
-      .post("http://localhost:5000/upload", formData)
-      .then((res) => {
-        console.log(res.data.message);
-        setIsUploadErr(false);
-        setIsUpload(true);
-      })
-      .catch((err) => console.log(err));
+
+    try {
+      const res = await axios.post("http://localhost:5000/upload", formData);
+      console.log(res.data.message);
+      setFormData({...formData, isUploadErr: true})
+      setFormData({ ...formData, isUpload: true });
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   function handleCopy(text) {
@@ -94,29 +90,25 @@ export default function GenerateSignature() {
             onChange={handleFileChange}
           />
           <button
-            onClick={() => handleFileUpload()}
+            onClick={handleFileUpload}
             type="button"
             className="text-yellow mx-auto mb-1 w-fit rounded-lg bg-igreen px-3 py-1 text-sm font-bold text-white"
           >
             Upload
           </button>
-          {isUploadErr ? (
+          {formData.isUploadErr && (
             <div className="mx-auto text-xs font-medium text-red-500">
               Please upload your file :)
             </div>
-          ) : (
-            ""
           )}
-          {isUpload ? (
+          {formData.isUpload && (
             <p className="mx-auto text-sm font-semibold text-igreen">
               File uploaded !!
             </p>
-          ) : (
-            ""
           )}
         </div>
       </div>
-      <div className="mb-6 flex flex-col  gap-y-5 lg:gap-x-8">
+      <div className="mb-6 flex flex-col gap-y-5 lg:gap-x-8">
         <div className="flex flex-col gap-2 rounded-lg bg-white px-7 py-4 lg:px-12 lg:pb-8">
           <span className="mx-auto text-base font-bold text-ipurple lg:text-lg">
             Private Key
@@ -124,8 +116,10 @@ export default function GenerateSignature() {
           <textarea
             rows="4"
             className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-6 py-3 text-sm"
-            value={inputPrivateKey}
-            onChange={(event) => setInputPrivateKey(event.target.value)}
+            value={formData.inputPrivateKey}
+            onChange={(event) =>
+              setFormData({ ...formData, inputPrivateKey: event.target.value })
+            }
             placeholder="Input your private key"
           />
         </div>
@@ -142,11 +136,11 @@ export default function GenerateSignature() {
             readOnly
             placeholder="Signature will be generated here"
           />
-          {isGeneratedSign ? (
+          {isGeneratedSign && (
             <a href={imageUrl} download className="mx-auto mb-2 h-full">
               <img src={imageUrl} alt="QRCODE" />
             </a>
-          ) : null}
+          )}
           <button
             onClick={() => handleCopy(signature)}
             className="text-yellow mx-auto w-fit rounded-lg bg-igreen px-3 py-1 text-sm font-bold text-white"
@@ -162,14 +156,12 @@ export default function GenerateSignature() {
       >
         Generate
       </button>
-      {isGeneratedSign ? (
+      {isGeneratedSign && (
         <div className=" mx-auto w-fit rounded-xl border-2 border-iyellow px-3 py-[0.2rem]">
           <p className=" font-semibold text-white">
             Signature & QR code generated !!
           </p>
         </div>
-      ) : (
-        ""
       )}
     </div>
   );
